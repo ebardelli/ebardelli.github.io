795a71e5c598b4d52fc5a858cec70e97ac334a91
ebardelli <bardelli@umich.edu>
2020-11-16 14:03:01 +0000

    Add APPAM presentation

---
+++ js/wowchemy-init.js
@@ -0,0 +1,49 @@
+(() => {
+  // ns-hugo:/home/runner/work/ebardelli.github.io/ebardelli.github.io/themes/academic/wowchemy/assets/js/wowchemy-theming.js
+  function getThemeMode() {
+    return parseInt(localStorage.getItem("dark_mode") || 2);
+  }
+  function canChangeTheme() {
+    return Boolean(window.wc.darkLightEnabled);
+  }
+  function initThemeVariation() {
+    if (!canChangeTheme()) {
+      return;
+    }
+    let currentThemeMode = getThemeMode();
+    let isDarkTheme;
+    switch (currentThemeMode) {
+      case 0:
+        isDarkTheme = false;
+        break;
+      case 1:
+        isDarkTheme = true;
+        break;
+      default:
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          isDarkTheme = true;
+        } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+          isDarkTheme = false;
+        } else {
+          isDarkTheme = window.wc.isSiteThemeDark;
+        }
+        break;
+    }
+    if (isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+
+  // ns-params:@params
+  var wcDarkLightEnabled = true;
+  var wcIsSiteThemeDark = false;
+
+  // js/wowchemy-init.js
+  window.wc = {
+    darkLightEnabled: wcDarkLightEnabled,
+    isSiteThemeDark: wcIsSiteThemeDark
+  };
+  initThemeVariation();
+})();
